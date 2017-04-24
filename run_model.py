82edc096d4e556d73de94912f1d3f3b5aaa9602e
samhitapn <samhitapn.96@gmail.com>
2017-04-24 19:40:17 +0200

    Add files via upload

--- run_model.py
+++ run_model.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """Reproduce your result by your saved model.
-
 This is a script that helps reproduce your prediction results using your saved
 model. This script is unfinished and you need to fill in to make this script
 work. If you are using R, please use the R script template instead.
-
 The script needs to work by typing the following commandline (file names can be
 different):
-
 python3 run_model.py -i unlabelled_sample.txt -m model.pkl -o output.txt
-
 """
 
 # author: Chao (Cico) Zhang
@@ -23,15 +19,26 @@ import pandas as pd
 
 # Start your coding
 
-# import the library you need here
-def import_samples(): 
-	#input for this should be unlabelled data file
-	data=pd.read_table('Complied-Data.txt', sep='\t', delimiter=None, delim_whitespace=False, header=0, index_col=0)
-	X = (data.iloc[0:100, 1:2835]) #NB: 150 is feature 1:149 for now, because of the long running time
-	return (X)
-
-# End your coding
-
+def parse_samples(inputFile): 
+    """ To parse the input file to required format."""
+    # Reading in the file and transpose the data frame.
+    data=pd.read_table(inputFile, sep='\t', delimiter=None, delim_whitespace=False, header=0, index_col=0)
+    data = data.transpose()
+    
+    # Modifying the columns
+    columnList = list(data.columns.values)
+    newColumnNames = []
+    for i in range(0,len(data.iloc[0])):
+        newColumnNames.append(str(data.iloc[0,i]) + "-" + str(data.iloc[1,i]))
+    data.columns=newColumnNames
+    data.insert(0,"Groups","")
+    data = data.drop(data.index[[0,1,2]])
+    
+    # Getting the required parts of the data
+    i = len(data)
+    j = len(data.iloc[0])
+    X = (data.iloc[0:i, 1:j]) #NB: 150 is feature 1:149 for now, because of the long running time
+    return (X)
 
 def main():
     """Main function."""
@@ -58,40 +65,40 @@ def main():
         sys.exit('Output is not designated!')
 
     # Start your coding
-    # suggested steps
-    # Step 1: load the model from the model file
-    # Step 2: apply the model to the input file to do the prediction
-    # Step 3: write the prediction into the desinated output file
-    output=str(sys.argv[3])	
-    model_file=str(sys.argv[2])
-    X=import_samples()
-    model = joblib.load('model.pkl') 
-    predictions=(model.predict(X))
-    predictions=predictions.tolist()
 
-    #changes output labels (0,1,2) to cancer type labels
+    # Getting the values from arguement parser
+    outputFile = args.output_file
+    model_file = args.model_file
+    inputFile = args.input_file
+    
+    # Parsing the input data
+    X = parse_samples(inputFile)
+        
+    # Predictions using the model
+    model = joblib.load(model_file) 
+    predictions = (model.predict(X))
+    predictions = predictions.tolist()
+
+    # Changing output labels (0,1,2) to cancer type labels
     for i in range(0,len(predictions)):
-    	if predictions[i]==0:
-    		predictions[i]="HER2+"
-    	elif predictions[i]==1:
-    		predictions[i]="HR+"
-    	elif predictions[i]==2:
-    		predictions[i]="Triple negative"
+        if predictions[i]==0:
+            predictions[i]="HER2+"
+        elif predictions[i]==1:
+            predictions[i]="HR+"
+        elif predictions[i]==2:
+            predictions[i]="Triple negative"
 
-    #writing to output file			
-    outputFile = open(output,"w")		
+    # Writing to output file			
+    outputFile = open(outputFile,"w")
+    outputFile.write('"Sample"' + "\t" + '"SubGroup"' + "\n")	
     for i in range(0,len(X.index)):
-    	outputFile.write(X.index[i] + "\t" + predictions[i] + "\n")
-    	print (X.index[i], predictions[i])
+        array = '"' + X.index[i] + '"'
+        predicted = '"' + predictions[i] + '"'
+        outputFile.write(array + "\t" + predicted + "\n")
     outputFile.close()	
-   
-
-    
-    
-    
-
+  
     # End your coding
-
 
 if __name__ == '__main__':
     main()
+
